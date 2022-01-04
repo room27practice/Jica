836702d0466a94c21b845151cb6c39351d5123cd
a <b>
2022-01-04 12:36:44 +0200

    Initial Commit

---
+++ Application/Application/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Application.Areas.Identity.IdentityHostingStartup))]
+namespace Application.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ Application/Common/Extensions/MapExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace Common.Extensions
+{
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using AutoMapperConfiguration;
+    using System.Linq;
+
+    public static class MapExtensions
+    {
+
+        private static MapperConfiguration mapcfg;
+        static MapExtensions()
+        {
+
+            mapcfg = new MapperConfiguration(mc =>
+             {
+                 mc.AddProfile(new MappingProfile());
+             });
+        }
+        public static IQueryable<TDestination> To<TDestination>(this IQueryable query)
+        {
+            return query.ProjectTo<TDestination>(mapcfg);
+        }
+    }
+}
